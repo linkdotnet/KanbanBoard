4ce8c8594bf4ecce518d962171e96194ec32de05
Steven Giesel <stgiesel35@gmail.com>
2020-09-29 20:34:10 +0200

    Added swimlane infrastructure

--- LinkDotNet.KanbanBoard.Domain/Goal.cs
+++ LinkDotNet.KanbanBoard.Domain/Goal.cs
@@ -5,17 +5,19 @@ namespace LinkDotNet.KanbanBoard.Domain
 {
     public class Goal
     {
-        public Goal(string title, DateTime deadline, IEnumerable<Subtask> subtasks, Rank rank)
+        public Goal(string title, DateTime deadline, IEnumerable<Subtask> subtasks, Rank rank, GoalStatus goalStatus)
         {
             Title = title;
             Deadline = deadline;
             Subtasks = subtasks;
             Rank = rank;
+            GoalStatus = goalStatus;
         }
 
         public string Title { get; private set; }
         public DateTime Deadline { get; private set; }
         public IEnumerable<Subtask> Subtasks { get; private set; }
         public Rank Rank { get; private set; }
+        public GoalStatus GoalStatus { get; private set; }
     }
 }
---
+++ LinkDotNet.KanbanBoard.Domain/GoalStatus.cs
@@ -0,0 +1,15 @@
+﻿using LinkDotNet.EnumValueObject;
+
+namespace LinkDotNet.KanbanBoard.Domain
+{
+    public class GoalStatus : EnumValueObject<GoalStatus>
+    {
+        public static readonly GoalStatus Todo = new GoalStatus(nameof(Todo));
+        public static readonly GoalStatus InProgress = new GoalStatus("In Progress");
+        public static readonly GoalStatus Completed = new GoalStatus(nameof(Completed));
+
+        private GoalStatus(string key) : base(key)
+        {
+        }
+    }
+}
