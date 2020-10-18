bc44a0dffb4f9a97012a2de94581a60e943793cb
Steven Giesel <stgiesel35@gmail.com>
2020-10-18 18:42:13 +0200

    Added Delete Action

--- LinkDotNet.KanbanBoard.UI/Features/GoalHandler.cs
+++ LinkDotNet.KanbanBoard.UI/Features/GoalHandler.cs
@@ -6,6 +6,7 @@ using BlazorState;
 using LinkDotNet.KanbanBoard.UI.Shared;
 using LinkDotNet.KanbanBoard.Web;
 using MediatR;
+using TypeSupport.Extensions;
 
 namespace LinkDotNet.KanbanBoard.UI.Features
 {
@@ -14,7 +15,6 @@ namespace LinkDotNet.KanbanBoard.UI.Features
         public class LoadGoalsHandler : ActionHandlerBase<LoadGoalsAction>
         {
             private readonly Kanban.KanbanClient _kanbanClient;
-            private GoalState GoalState => Store.GetState<GoalState>();
 
             public LoadGoalsHandler(IStore store, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(store, toastService)
             {
@@ -34,9 +34,8 @@ namespace LinkDotNet.KanbanBoard.UI.Features
         public class AddGoalHandler : ActionHandlerBase<AddGoalAction>
         {
             private readonly Kanban.KanbanClient _kanbanClient;
-            private GoalState GoalState => Store.GetState<GoalState>();
 
-            public AddGoalHandler(IStore aStore, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(aStore, toastService)
+            public AddGoalHandler(IStore store, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(store, toastService)
             {
                 _kanbanClient = kanbanClient;
             }
@@ -52,21 +51,37 @@ namespace LinkDotNet.KanbanBoard.UI.Features
 
         public class ChangeGoalStatusHandler : ActionHandlerBase<ChangeGoalStatusAction>
         {
-            private readonly IStore _aStore;
             private readonly Kanban.KanbanClient _kanbanClient;
 
-            public ChangeGoalStatusHandler(IStore aStore, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(aStore, toastService)
+            public ChangeGoalStatusHandler(IStore store, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(store, toastService)
             {
-                _aStore = aStore;
                 _kanbanClient = kanbanClient;
             }
 
             protected override async Task<Unit> InnerHandleAsync(ChangeGoalStatusAction aAction, CancellationToken aCancellationToken)
             {
-                var goal = _aStore.GetState<GoalState>()._goals.Single(g => g.Id == aAction.Id);
+                var goal = GoalState._goals.Single(g => g.Id == aAction.Id);
                 goal.GoalStatus = aAction.NewStatus;
                 await _kanbanClient.UpdateGoalAsync(goal.ToGoalDto());
 
+                return Unit.Value;
+            }
+        }
+
+        public class DeleteGoalHandler : ActionHandlerBase<DeleteGoalAction>
+        {
+            private readonly Kanban.KanbanClient _kanbanClient;
+
+            public DeleteGoalHandler(IStore store, IToastService toastService, Kanban.KanbanClient kanbanClient) : base(store, toastService)
+            {
+                _kanbanClient = kanbanClient;
+            }
+
+            protected override async Task<Unit> InnerHandleAsync(DeleteGoalAction action, CancellationToken cancellationToken)
+            {
+                var goal = GoalState._goals.Single(g => g.Id == action.Id);
+                goal.IsDeleted = true;
+                await _kanbanClient.UpdateGoalAsync(goal.ToGoalDto());
                 return Unit.Value;
             }
         }
