e8fb545c7eba1ce4ad5bf946ea14591f5dae08f2
Steven Giesel <stgiesel35@gmail.com>
2020-11-06 15:48:07 +0100

    Added tests and ignored test which relies on bUnit Update

---
+++ LinkDotNet.KanbanBoard.UI.UnitTests/Domain/GoalTests.cs
@@ -0,0 +1,67 @@
+﻿using LinkDotNet.KanbanBoard.Domain;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+using System;
+
+namespace LinkDotNet.KanbanBoard.UI.UnitTests.Domain
+{
+    [TestClass]
+    public class GoalTests
+    {
+        [TestMethod]
+        public void GivenValidInput_WhenCreatingGoal_ThenSuccess()
+        {
+            var result = Goal.Create("Title", Rank.ImportantAndUrgent, GoalStatus.Todo, null, null);
+
+            result.IsSuccess.ShouldBeTrue();
+            var goal = result.Value;
+            goal.Title.ShouldBe("Title");
+            goal.Rank.ShouldBe(Rank.ImportantAndUrgent);
+            goal.GoalStatus.ShouldBe(GoalStatus.Todo);
+            goal.HasDeadline.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void GivenGoal_WhenCreatingWithEmptyTitle_ThenFailure()
+        {
+            var result = Goal.Create(string.Empty, Rank.Important, GoalStatus.Completed, null, null);
+
+            result.IsFailure.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void GivenGoal_WhenCreatingWithNullRank_ThenFailure()
+        {
+            var result = Goal.Create("Title", null, GoalStatus.Completed, null, null);
+
+            result.IsFailure.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void GivenGoal_WhenCreatingWithNullGoalStatus_ThenFailure()
+        {
+            var result = Goal.Create("Title", Rank.Important, null, null, null);
+
+            result.IsFailure.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void GivenDeadlineInPast_WhenGettingIsOverdue_ThenTrze()
+        {
+            var goal = new GoalBuilder().WithDeadline(DateTime.Now.AddDays(-1)).Build();
+
+            goal.IsOverdue.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void GivenDeadlineInFuture_WhenGettingIsOverdue_ThenTrze()
+        {
+            var goal = new GoalBuilder().WithDeadline(DateTime.Now.AddDays(1)).Build();
+
+            goal.IsOverdue.ShouldBeFalse();
+        }
+    }
+}
---
+++ LinkDotNet.KanbanBoard.UI.UnitTests/Domain/LabelTests.cs
@@ -0,0 +1,43 @@
+﻿using LinkDotNet.KanbanBoard.Domain;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+namespace LinkDotNet.KanbanBoard.UI.UnitTests.Domain
+{
+    [TestClass]
+    public class LabelTests
+    {
+        [TestMethod]
+        public void GivenLabel_WhenCreatingWithValidValues_ThenSuccess()
+        {
+            var result = Label.Create("Work", "#332211");
+
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.LabelName.ShouldBe("Work");
+            result.Value.HexColor.ShouldBe("#332211");
+        }
+
+        [TestMethod]
+        public void GivenLabel_WhenCreatingWithEmptyTitle_ThenFailure()
+        {
+            var result = Label.Create(string.Empty, "#121212");
+
+            result.IsFailure.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        [DataRow("121212")]
+        [DataRow("green")]
+        [DataRow("#1234567")]
+        [DataRow("#123")]
+        [DataRow("123")]
+        public void GivenLabel_WhenCreatingWithInvalidHexColor_ThenFailure(string hexColor)
+        {
+            var result = Label.Create(string.Empty, hexColor);
+
+            result.IsFailure.ShouldBeTrue();
+        }
+    }
+}
